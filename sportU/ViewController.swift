259c4d92d7cc0da090d529a80055f5aa9b24ef11
Miles Hinson <mileshinson@Miless-MacBook-Pro.local>
2015-08-25 12:40:16 -0400

    sportU initial version

---
+++ sportU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sportU
+//
+//  Created by Miles Hinson on 8/25/15.
+//  Copyright (c) 2015 sportU Princeton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
